657a7152f8d593c09f0a813b617b2d2f374003aa
Vinod-Rajapaksha <madurangavinod758@gmail.com>
2025-08-17 17:08:16 +0530

    import : packages entity and JpaRepository
    Co-Authored-By: IT24100599 <176550650+IT24100599@users.noreply.github.com>

--- backend/src/main/java/com/backend/repository/ReservationRepository.java
+++ backend/src/main/java/com/backend/repository/ReservationRepository.java
@@ -1,5 +1,7 @@
 package com.backend.repository;
 
+import com.backend.entity.Reservation;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
