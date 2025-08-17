aae02071c3d9ead03edc7bb1ce9cd53f0bd1c2be
Vinod-Rajapaksha <madurangavinod758@gmail.com>
2025-08-17 09:00:44 +0530

    implement : admin model
    Co-Authored-By: IT24100599 <176550650+IT24100599@users.noreply.github.com>

---
+++ backend/src/main/java/com/backend/entity/Admin.java
@@ -0,0 +1,28 @@
+package com.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "Admin")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Admin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long adminID;
+
+    private String fName;
+    private String lName;
+    private String role;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String passwordHash;
+    private String phone;
+    private LocalDateTime createdAt = LocalDateTime.now();
+}
