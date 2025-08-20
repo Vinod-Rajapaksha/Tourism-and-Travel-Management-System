1067f924e1fba766f16bc1bf1978ad6089d5d26e
Vinod-Rajapaksha <madurangavinod758@gmail.com>
2025-08-21 01:56:40 +0530

    update : auth layout
    Co-Authored-By: IT24100599 <176550650+IT24100599@users.noreply.github.com>

--- frontend/src/layouts/AuthLayout.jsx
+++ frontend/src/layouts/AuthLayout.jsx
@@ -1,17 +1,10 @@
+import React from "react";
 import { Outlet } from "react-router-dom";
 
 export default function AuthLayout() {
-  return (
-    <div className="container py-5">
-      <div className="row justify-content-center">
-        <div className="col-12 col-md-8 col-lg-5">
-          <div className="card shadow-sm">
-            <div className="card-body p-4">
-              <Outlet />
-            </div>
-          </div>
+    return (
+        <div className="container">
+        <Outlet />
         </div>
-      </div>
-    </div>
-  );
+    );
 }
