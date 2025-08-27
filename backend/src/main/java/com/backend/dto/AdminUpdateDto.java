11ad6bff1c68895ee927dacf9ffbe6000740b992
Vinod-Rajapaksha <madurangavinod758@gmail.com>
2025-08-27 16:17:48 +0530

    implement : admin profile update dto
    Co-Authored-By: IT24100599 <176550650+IT24100599@users.noreply.github.com>

---
+++ backend/src/main/java/com/backend/dto/AdminUpdateDto.java
@@ -0,0 +1,15 @@
+package com.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AdminUpdateDto {
+    private String fName;
+    private String lName;
+    private String phone;
+    private String password;
+}
