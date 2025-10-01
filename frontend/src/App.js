997550607f80e1bb3938decec66cb7e33a7fe01a
Ranuda Perera <it24100688@my.sliit.lk>
2025-10-02 05:24:06 +0530

    add: protected dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,41 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import SeniorTravelConsultantDashboard from "./pages/dashboard/SeniorTravelConsultantDashboard";
 import PackagesList from "./pages/client/PackagesList";
+import Login from "./pages/auth/Login";
+import Logout from "./pages/auth/Logout";
+import RoleBasedDashboardRedirect from "./components/RoleBasedDashboardRedirect";
+import RoleRoute from "./routes/RoleRoute";
+import ProtectedRoute from "./routes/ProtectedRoute";
 
 function App() {
   return (
     <Routes>
       {/* Customer side */}
       <Route path="/packages" element={<PackagesList />} />
+      {/* Public login */}
 
-      {/* Senior Travel Consultant dashboard */}
+      <Route path="/login" element={<Login />} />
+
+      {/* protected entry point: auto redirects by role */}
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <RoleBasedDashboardRedirect />
+          </ProtectedRoute>
+        }
+      />  
+
+      {/* Example: Senior Travel Consultant role only */}
       <Route
         path="/dashboard/senior-travel-consultant"
-        element={<SeniorTravelConsultantDashboard />}
+        element={
+          <ProtectedRoute>
+            <RoleRoute roles={["SENIOR_TRAVEL_CONSULTANT"]}>
+              <SeniorTravelConsultantDashboard />
+            </RoleRoute>
+          </ProtectedRoute>
+        }
       />
 
       {/* Default: if no route matches, go to customer packages */}
