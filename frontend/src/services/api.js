e8a5272d09885bfaf0c9656da18b2a8e9661160a
ranuda-perera <ranudaperera@gmail.com>
2025-10-14 06:57:18 +0530

    add: interceptor for tokens

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -5,7 +5,9 @@ const api = axios.create({ baseURL: process.env.REACT_APP_API_BASE_URL || "http:
 // attach token
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
   return config;
 });
 
